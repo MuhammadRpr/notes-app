5acf8349dd53c92721291f6e0c6396c4e69458bc
MuhammadRpr <muhammadqu017@gmail.com>
2025-09-26 10:34:01 +0700

    delete

--- backend/src/handlers/notesHandler.js
+++ backend/src/handlers/notesHandler.js
@@ -15,15 +15,34 @@ export const getAllNotesHandler = async (req, res) => {
 export const addNoteHandler = async (req, res) => {
     const { title, content } = req.body;
 
+    if (!title || !title.trim()) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Title and content are required',
+        });
+    }
+
+    if (!content || !content.trim()) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Title and content are required',
+        });
+    }
+
     const [insertResult] = await pool.query(
         'INSERT INTO notes (title, content) VALUES (?, ?)',
         [title, content]
 
     );
 
+    const [notes] = await pool.query('SELECT * FROM notes WHERE id = ?', [
+        insertResult.insertId
+    ]);
+
     res.status(201).json({
         status: 'success',
         message: 'Note added successfully',
+        data: notes[0],
 
     });
 };
@@ -34,8 +53,73 @@ export const getNoteByIdHandler = async (req, res) => {
 
     const [notes] = await pool.query('SELECT * FROM notes WHERE id = ?', [id]);
 
+    if (notes.length === 0) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Note not found',
+        });
+    }
+
+
     res.status(200).json({
         status: 'success',
         data: notes[0],
     });
 };
+
+export const updateNoteByIdHandler = async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    if (!title || !title.trim()) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Title and content are required',
+        });
+    }
+
+    if (!content || !content.trim()) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Title and content are required',
+        });
+    }
+
+    await pool.query('UPDATE notes set title=?, content=? WHERE id=?',
+        [title, content, id]);
+
+    const [notes] = await pool.query('SELECT * FROM notes WHERE id = ?', [id]);
+
+    if (notes.length === 0) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Note not found',
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Note updated successfully',
+        data: notes[0]
+    });
+
+
+}
+
+export const deleteNoteByIdHandler = async (req, res) => {
+    const { id } = req.params;
+
+    const [deleteNote] = await pool.query('DELETE FROM notes WHERE id = ?', [id]);
+
+    if (deleteNote.affectedRows === 0) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Note not found',
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Note deleted successfully',
+    });
+}
--- backend/src/routes/notesRoute.js
+++ backend/src/routes/notesRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } from '../handlers/notesHandler.js';
+import { addNoteHandler, deleteNoteByIdHandler, getAllNotesHandler, getNoteByIdHandler, updateNoteByIdHandler } from '../handlers/notesHandler.js';
 
 const notesRouter = express.Router();
 
 notesRouter.get('/notes', getAllNotesHandler);
 notesRouter.post('/notes', addNoteHandler);
 notesRouter.get('/notes/:id', getNoteByIdHandler);
+notesRouter.put('/notes/:id', updateNoteByIdHandler);
+notesRouter.delete('/notes/:id', deleteNoteByIdHandler);
+
 
 export default notesRouter;

eabcac78b677662c8384957a262907ea4a312e58
MuhammadRpr <muhammadqu017@gmail.com>
2025-10-08 11:53:46 +0700

    lgi

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
     return (
-        <nav className="w-full fixed top-0 flex justify-center bg-orange-500 p-1">
+        <nav className="w-full fixed top-0 flex justify-center bg-orange-500 p-2 pl-3">
             <div className="flex justify-between px-1 container">
                 <img src="/Group 21.svg" alt="Logo" className="w-15 h-15" />
             </div>
